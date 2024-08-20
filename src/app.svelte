90424f8a74ee58e0430ac9f881d1b0e3d1a4f0aa
PromiSe#### <vicimpa@ya.ru>
2024-08-20 15:39:30 +0700

    Append new logo

--- src/app.svelte
+++ src/app.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import Logo from "$components/Logo.svelte";
-  import logoSrc from "$images/logo.png";
+  import logoSrc from "$images/roskosmos.png";
 </script>
 
-<Logo src={logoSrc} size={1} padding={150} p={2} r={200} a={0.2} />
+<Logo src={logoSrc} size={0.2} padding={70} p={1} r={200} a={0.7} />
