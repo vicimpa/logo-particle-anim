6a963bbf6d12b289e69ad68f931d8ff13fc3667e
PromiSe#### <vicimpa@ya.ru>
2025-08-20 19:08:41 +0300

    Fix

--- src/app.svelte
+++ src/app.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import Logo from "$components/Logo.svelte";
-  import logoSrc from "$images/gerb.png";
+  import logoSrc from "$images/icon.png";
 </script>
 
-<Logo src={logoSrc} size={0.7} p={1} padding={100} r={250} a={0.6} />
+<Logo src={logoSrc} size={1} p={1} padding={100} r={250} a={0.2} />
